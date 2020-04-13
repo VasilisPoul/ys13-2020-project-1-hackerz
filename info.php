fe2177d5ad82720e79f99943ca47f88e3cc47354
vasilisp100 <vasilis.p.100@gmail.com>
2020-04-13 18:10:38 +0300

    info.php
    Co-authored-by: Kostas Hatzopoulos <kwstarikanos@gmail.com>

--- info.php
+++ info.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo();
+//phpinfo();
+$urlServer = $_GET['x'];
+
+$validation = "/^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i";
+if ((bool)preg_match($validation, $urlServer) === false) {
+    $urlServer =  "hack busted!";//_SERVER["REQUEST_SCHEME"] . "://" . _SERVER["HTTP_HOST"] . "/";
+    echo $urlServer;
+}
+
+
 ?>
